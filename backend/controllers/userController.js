11229d5e2cb016928af205b54df42ec4535c11e6
Ansh Gera <anshgera947@gmail.com>
2025-06-12 17:41:57 +0530

    Add logout functionality to userController and update userRoute

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,3 +75,12 @@ export const login = async(req, res)=>{
         return res.status(500).json({message: "Internal server error"});
     }
 };
+
+export const logout = (req, res)=>{
+    try{
+        return res.status(200).cookie("token", "", {maxAge: 0}).json({message: "User logged out successfully"});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message: "Internal server error"});
+    }
+}
